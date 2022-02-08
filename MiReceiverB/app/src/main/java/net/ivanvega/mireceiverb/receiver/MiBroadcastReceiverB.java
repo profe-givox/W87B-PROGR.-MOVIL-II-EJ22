22707a30bf87d9f4f782e626ffc4c8f331436b1b
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2022-02-08 14:50:02 -0600

    Se agrego el reepor a nivel de manifesto SMS_RECEIVED_ACTION

--- MiReceiverB/app/src/main/java/net/ivanvega/mireceiverb/receiver/MiBroadcastReceiverB.java
+++ MiReceiverB/app/src/main/java/net/ivanvega/mireceiverb/receiver/MiBroadcastReceiverB.java
@@ -3,18 +3,37 @@ package net.ivanvega.mireceiverb.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.Telephony;
+import android.telephony.SmsMessage;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MiBroadcastReceiverB
         extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-            Log.d("MisActions", intent.getAction());
 
+            String actionStr = intent.getAction();
+            Log.d("MIBROADCASTB", actionStr);
+        
             if (intent.getAction().equals(
-                    Telephony.Sms.Intents.SMS_REJECTED_ACTION)){
+                    Telephony.Sms.Intents.SMS_RECEIVED_ACTION)){
+
+                 Bundle sms = intent.getExtras();
+
+                 Object[] pdus  = (Object[])sms.get("pdus");
+
+                 String mensaje = "Numero: ";
+                 for (Object message : pdus){
+                     SmsMessage men = SmsMessage.createFromPdu((byte[]) message);
+                      mensaje += men.getOriginatingAddress();
+                      mensaje += "\n"+ men.getMessageBody();
+                 }
+
+                 Log.d("MIBROADCASTB", mensaje);
+                Toast.makeText(context, mensaje , Toast.LENGTH_SHORT).show();
 
             }
 

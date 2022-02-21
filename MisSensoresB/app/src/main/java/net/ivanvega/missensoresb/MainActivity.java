9b1b0973f0c2ebd175c48daa088f16577c02c5ea
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2022-02-21 14:40:57 -0600

    Se agrego sensor de Luz

--- MisSensoresB/app/src/main/java/net/ivanvega/missensoresb/MainActivity.java
+++ MisSensoresB/app/src/main/java/net/ivanvega/missensoresb/MainActivity.java
@@ -4,20 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private SensorManager sensorManager;
+    private Sensor sensorLuz;
+
+    TextView txtLuz, txtProximidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtLuz = findViewById(R.id.txtLuz);
 
          sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
@@ -28,5 +36,37 @@ public class MainActivity extends AppCompatActivity {
              Log.d("XENSOR", sensor.toString());
          }
 
+         sensorLuz =
+                 sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+    }
+
+    SensorEventListener sensorEventListenerLuz = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            String mes = "Sendor de luz: " + sensorEvent.values[0];
+            txtLuz.setText(mes);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+    } ;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorLuz!=null) {
+            sensorManager.registerListener(sensorEventListenerLuz,
+                    sensorLuz,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListenerLuz);
     }
 }

b761b69a074a7c07d043d52f960c73cadd05e2d9
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2022-03-02 14:52:13 -0600

    SE AGREGO LA ULTIMA UBICACION CONOCIDA

--- MiUbicacionYMapas/app/src/main/java/net/ivanvega/miubicacionymapas/MainActivity.java
+++ MiUbicacionYMapas/app/src/main/java/net/ivanvega/miubicacionymapas/MainActivity.java
@@ -2,17 +2,30 @@ package net.ivanvega.miubicacionymapas;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.CancellationToken;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.OnTokenCanceledListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -22,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
 
         validarPermisos();
 
-        solicitarPermiso();
 
     }
 
@@ -31,14 +43,14 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(
                 this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
                 PackageManager.PERMISSION_GRANTED
-        &&
+                &&
                 ContextCompat.checkSelfPermission(
                         this, Manifest.permission.ACCESS_FINE_LOCATION) !=
                         PackageManager.PERMISSION_GRANTED
         ) {
             // You can use the API that requires the permission.
             solicitarPermiso();
-        }else{
+        } else {
             ultimaUbicacionConcedida();
         }
     }
@@ -54,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                             Boolean fineLocationGranted = result.getOrDefault(
                                     Manifest.permission.ACCESS_FINE_LOCATION, false);
                             Boolean coarseLocationGranted = result.getOrDefault(
-                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                                    Manifest.permission.ACCESS_COARSE_LOCATION, false);
                             if (fineLocationGranted != null && fineLocationGranted) {
                                 // Precise location access granted.
                                 ultimaUbicacionConcedida();
@@ -72,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         // Before you perform the actual permission request, check whether your app
         // already has the permissions, and whether your app needs to show a permission
         // rationale dialog. For more details, see Request permissions.
-        locationPermissionRequest.launch(new String[] {
+        locationPermissionRequest.launch(new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION
         });
@@ -80,5 +92,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void ultimaUbicacionConcedida() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    // Logic to handle location object
+                    String loca = location.getLatitude() + ", " + location.getLongitude();
+                    Log.d("UBIX", loca);
+                    Toast.makeText(getApplicationContext(), loca,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        fusedLocationClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY,
+                new CancellationToken() {
+                    @NonNull
+                    @Override
+                    public CancellationToken onCanceledRequested(@NonNull OnTokenCanceledListener onTokenCanceledListener) {
+                        return null;
+                    }
+
+                    @Override
+                    public boolean isCancellationRequested() {
+                        return false;
+                    }
+                }
+        ).addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // Got last known location. In some rare situations this can be null.
+                if (location != null) {
+                    // Logic to handle location object
+                    String loca = location.getLatitude() + ", " + location.getLongitude();
+                    Log.d("UBIXC", loca);
+                    Toast.makeText(getApplicationContext(), loca,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

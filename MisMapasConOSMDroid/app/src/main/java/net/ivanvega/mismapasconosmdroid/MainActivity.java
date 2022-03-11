b75fbb13cb86acd920a862f9899eb27fb8f81167
Gustavo Ivan Vega Olvera <sproveeduria@charly.com.mx>
2022-03-11 15:05:56 -0600

    se agrego la peticion a la API open map rpute con volley

--- MisMapasConOSMDroid/app/src/main/java/net/ivanvega/mismapasconosmdroid/MainActivity.java
+++ MisMapasConOSMDroid/app/src/main/java/net/ivanvega/mismapasconosmdroid/MainActivity.java
@@ -2,18 +2,34 @@ package net.ivanvega.mismapasconosmdroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Polyline;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     MapView map = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +58,74 @@ public class MainActivity extends AppCompatActivity {
         mapController.setZoom(17.0);
         GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
         mapController.setCenter(startPoint);
+
+        dibujarPolilinea();
+
+    }
+
+    private void obtenerRouteFromMapRequest(){
+        RequestQueue queue =
+                Volley.newRequestQueue(this);
+
+        JsonObjectRequest requestMapRequest =
+                new JsonObjectRequest(
+                        "http://www.mapquestapi.com/directions/v2/route?key=rHGSMo2PbPzaZi281CA44gjIYcqOpJ6m&from=20.14649016556056,-101.17566401392817&to=20.126496094732943,-101.19317063853653",
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONArray   indicaiones =  response.getJSONObject("route")
+                                            .getJSONObject("legs")
+                                            .getJSONArray("maneuvers");
+
+
+
+                                    for( int i =0 ;  i <indicaiones.length(); i++){
+                                        JSONObject indi = indicaiones.getJSONObject(i);
+                                        indi.getJSONObject("startPoint").get("lat");
+                                        indi.getJSONObject("startPoint").get("lng");
+
+                                        
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                );
+    }
+
+    private void dibujarPolilinea() {
+
+        List<GeoPoint> geoPoints = new ArrayList<>();
+        //add your points here
+        geoPoints.add(new GeoPoint(20.14031364415948d, -101.15067348127037d));
+        geoPoints.add(new GeoPoint(20.143340053116482d, -101.14987627528735d));
+
+
+        Polyline line = new Polyline();   //see note below!
+        line.setPoints(geoPoints);
+        line.setOnClickListener(new Polyline.OnClickListener() {
+            @Override
+            public boolean onClick(Polyline polyline, MapView mapView, GeoPoint eventPos) {
+                Toast.makeText(mapView.getContext(), "polyline with " + polyline.getPoints().size() + "pts was tapped", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+        map.getOverlayManager().add(line);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(17.0);
+        GeoPoint startPoint = new GeoPoint(20.14031364415948d, -101.15067348127037d);
+        mapController.setCenter(startPoint);
+
+
 
     }
 
